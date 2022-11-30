ae79b91df3a8c28f12dce4751e6554a8235456dc
Gabbynts <gabriella.natasya2003@gmail.com>
2022-11-30 14:10:38 +0700

    feat: CrashDodgerButton

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,5 +1,35 @@
 package view;
 
-public class ViewManager {
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import model.CrashDodgerButton;
 
+public class ViewManager {
+	
+	private static final int HEIGHT = 600;
+	private static final int WIDTH = 800;
+	private AnchorPane mainPane;
+	private Scene mainScene;
+	private Stage mainStage;
+	
+	public ViewManager() {
+		mainPane = new AnchorPane();
+		mainScene = new Scene(mainPane, WIDTH, HEIGHT);
+		mainStage = new Stage();
+		mainStage.setScene(mainScene);
+		createButtons();
+	}
+	
+	public Stage getMainStage() {
+		return mainStage;
+	}
+	
+	private void createButtons() {
+		CrashDodgerButton button = new CrashDodgerButton("Click me!");
+		mainPane.getChildren().add(button);	
+	}
 }
