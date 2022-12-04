2c64162a683737ac55d142d2424f2787f1804b00
Gabbynts <gabriella.natasya2003@gmail.com>
2022-12-04 15:46:44 +0700

    feat: buttons

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -10,18 +13,25 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
 import model.CrashDodgerButton;
 
 public class ViewManager {
 	
-	private static final int HEIGHT = 600;
-	private static final int WIDTH = 800;
+	private static final int HEIGHT = 768;
+	private static final int WIDTH = 1024;
 	private AnchorPane mainPane;
 	private Scene mainScene;
 	private Stage mainStage;
 	
+	private final static int MENU_BUTTONS_START_X = 100;
+	private final static int MENU_BUTTONS_START_Y = 150;
+	
+	List<CrashDodgerButton> menuButtons;
+	
 	public ViewManager() {
+		menuButtons = new ArrayList<>();
 		mainPane = new AnchorPane();
 		mainScene = new Scene(mainPane, WIDTH, HEIGHT);
 		mainStage = new Stage();
@@ -34,16 +44,48 @@ public class ViewManager {
 		return mainStage;
 	}
 	
+	private void addMenuButton(CrashDodgerButton button) {
+		button.setLayoutX(MENU_BUTTONS_START_X);
+		button.setLayoutY(MENU_BUTTONS_START_Y + menuButtons.size() * 100);
+		menuButtons.add(button);
+		mainPane.getChildren().add(button);
+	}
+	
 	private void createButtons() {
-		CrashDodgerButton button = new CrashDodgerButton("Click me!");
-		mainPane.getChildren().add(button);	
-		
-		button.setLayoutX(200);
-		button.setLayoutY(200);
+		createStartButton();
+		createScoresButton();
+		createHelpButton();
+		createCreditsButton();
+		createExitButton();
+	}
+	
+	private void createStartButton() {
+		CrashDodgerButton startButton = new CrashDodgerButton("PLAY");
+		addMenuButton(startButton);
+	}
+	
+	private void createScoresButton() {
+		CrashDodgerButton scoresButton = new CrashDodgerButton("SCORES");
+		addMenuButton(scoresButton);
+	}
+	
+	private void createHelpButton() {
+		CrashDodgerButton helpButton = new CrashDodgerButton("HELP");
+		addMenuButton(helpButton);
+	}
+	
+	private void createCreditsButton() {
+		CrashDodgerButton creditsButton = new CrashDodgerButton("CREDITS");
+		addMenuButton(creditsButton);
+	}
+	
+	private void createExitButton() {
+		CrashDodgerButton exitButton = new CrashDodgerButton("EXIT");
+		addMenuButton(exitButton);
 	}
 	
 	private void createBackground() {
-		Image backgroundImage = new Image(getClass().getResourceAsStream("resources/purple.png"));
+		Image backgroundImage = new Image(getClass().getResourceAsStream("resources/purple.jpg"));
 		BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
 		mainPane.setBackground(new Background(background));
 	}
